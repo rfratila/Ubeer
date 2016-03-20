8af8f08d5ff36ec50c2dd2c766d24d7785403f2f
muntashir <muntashir.hossain@uwaterloo.ca>
2016-03-20 05:31:52 -0400

    relay

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var os = require('os');
 var ifaces = os.networkInterfaces();
 var host;
 var port = 1234;
+var forwardingSockets = [];
 
 Object.keys(ifaces).forEach(function (ifname) {
   ifaces[ifname].forEach(function (iface) {
@@ -18,12 +19,29 @@ net.createServer(function (socket) {
 
   socket.on('data', function (data) {
     if (data) {
+      
+      if (data === 'ADD_RELAY') {
+        forwardingSockets.push(socket);
+        return;
+      } 
+      
+      if (data.split(':')[0] === 'EDISON') {
+        for (var i = 0; i < forwardingSockets.length; i++) {
+          socket.write(data.split(':')[1]);
+        }
+        return;
+      }
+      
       console.log('DATA ' + socket.remoteAddress + ': ' + data);
     }
   });
 
   socket.on('close', function (data) {
     console.log('CLOSED: ' + socket.remoteAddress + ' ' + socket.remotePort);
+  });
+  
+  socket.on('error', function (data) {
+    console.log('ERROR: ' + socket.remoteAddress + ' ' + socket.remotePort);
   });
 
 }).listen(port, host);
