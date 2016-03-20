63bd7902a9a3b92f13be1af3ac5321f482c2edab
Robert Fratila <fratilarobert@hotmail.com>
2016-03-20 10:40:40 -0400

    changed time to turn

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -161,8 +161,8 @@ void flashLED(int numTimes, int duration) {
 void forward(int speedR, int speedL) {
   digitalWrite(pinAO1, HIGH);
   digitalWrite(pinAO2, LOW);
-  digitalWrite(pinBO1, LOW);
-  digitalWrite(pinBO2, HIGH);
+  digitalWrite(pinBO1, HIGH);
+  digitalWrite(pinBO2, LOW);
 
   int  sigR = map(speedR, 1 , 100, 0, 255);
   int  sigL = map(speedL, 1 , 100, 0, 255);
